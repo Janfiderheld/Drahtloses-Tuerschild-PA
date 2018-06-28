1980b8f3de3c0cee2223fb16cc648dc1dedeb5af
Janfiderheld <Janfitoe@web.de>
2018-06-28 17:46:30 +0200

    Hausdörfer-Funktionalität im WI & Doku
    Doku geschrieben, nen Diagramm erstellt, die Hausdörfer-Funktionalität dem Webinterface hinzugefügt, den Entwurf überarbeitet und die PHP-Header eingerichtet. What a Projektarbeitsstunde!

--- PHP-Webserver/Webinterface/getDataFromCSV.php
+++ PHP-Webserver/Webinterface/getDataFromCSV.php
@@ -1,8 +1,36 @@
 <?php
-	// Name (& Speicherort) der CSV-Datei, über die die Daten ausgetauscht werden
-	$fileName = 'status.csv';		// Speicherort im gleichen Ort
-	$fileName = './status.csv';		// Speicherort einen Ordner darüber
-	// liest die Daten aus der CSV-Datei in das fileContentOld-Array
+	/**
+	*	Projektarbeit Sommersemester 2018 - Drahtloses Türschild mit E-Paper-Display
+	*	Author: Jan-Philipp Töberg
+	*	Version: 1.3
+	*	Letzte Änderung: 28.06.2018
+	*	
+	*	Dieses PHP-Skript regelt den Lesezugriff auf die unterschiedlichen CSV-Dateien.
+	*	Dabei steuert es, welche Werte geladen werden und übertragt diese an den Client per XMLHttp-Request.
+	**/
+
+	// Einstellen der internen Zeichenkodierung auf UTF-8 (wie bei HTML)
+	mb_internal_encoding("UTF-8");
+	// Auslesen des aktuellen Profs aus der Client-Übertragung
+	$currentProf = htmlentities($_POST['prof']);
+	
+	// wählt die Datei abhängig vom ausgewählten Professor aus
+	switch($currentProf) {
+		case 'korte':
+			// $fileName = 'Korte_status.csv';			// Speicherort im gleichen Ort
+			$fileName = '../Korte_status.csv';		// Speicherort einen Ordner darüber
+			break;
+		case 'hausdoerfer':
+			// $fileName = 'Hausdoerfer_status.csv';		// Speicherort im gleichen Ort
+			$fileName = '../Hausdoerfer_status.csv';		// Speicherort einen Ordner darüber
+			break;
+		default:
+			// $fileName = 'status.csv';		// Speicherort im gleichen Ort
+			$fileName = '../status.csv';		// Speicherort einen Ordner darüber
+			break;
+	}	
+
+	// öffnet die CSV-Datei, um Daten in das fileContentOld-Array zu lesen
 	$file = fopen($fileName, 'r');	
 	
 	/* fileContentOld ist ein Array:
@@ -13,7 +41,7 @@
 	$fileContentOld = fgetcsv($file, 1000, ",");
 	fclose($file);
 	
-	// Übergabe des Array-Inhalts als JSON formatiert an das Javascript-Skript 
+	// Übergabe des Array-Inhalts, als JSON formatiert, an das Javascript-Skript 
 	echo json_encode( array( 'status' => $fileContentOld[0], 'newsNew' => $fileContentOld[1], 'newsOld' => $fileContentOld[2] ) );
 	
 	// Beendet das PHP-Skript
--- PHP-Webserver/Webinterface/saveDataToCSV.php
+++ PHP-Webserver/Webinterface/saveDataToCSV.php
@@ -1,12 +1,20 @@
 <?php
-	echo "Übertragene Daten werden gespeichert...\n";
-	// Name (& Speicherort) der CSV-Datei, über die die Daten ausgetauscht werden
-	$fileName = 'status.csv';		// Speicherort im gleichen Ort
-	$fileName = './status.csv';		// Speicherort einen Ordner darüber
-	// Standard-Text aus dem TextArea der HTML-Datei
-	$defaultText = 'Geben Sie hier die neue Meldung ein!';
+	/**
+	*	Projektarbeit Sommersemester 2018 - Drahtloses Türschild mit E-Paper-Display
+	*	Author: Jan-Philipp Töberg
+	*	Version: 1.5
+	*	Letzte Änderung: 28.06.2018
+	*	
+	*	Dieses PHP-Skript regelt den Schreibzugriff auf die unterschiedlichen CSV-Dateien.
+	*	Dabei bekommt es Daten per XMLHttp-Request vom Client gesendet, welche es überprüft und anschließend
+	*	in der passenden Datei speichert.
+	**/
+
 	// Einstellen der internen Zeichenkodierung auf UTF-8 (wie bei HTML)
 	mb_internal_encoding("UTF-8");
+	
+	// Standard-Text in dem Eingabebereich
+	$defaultText = 'Geben Sie hier die neue Meldung ein';
 	
 	// die übertragenen Daten von JavaScript
 	$state = htmlentities($_POST['zustand']);
@@ -17,20 +25,39 @@
 	// Da die Datumsangabe genau 16 Zeichen lang ist, wird bei Zeichen 17 getrennt 
 	$newsNewWithoutDate = substr($newsNew, 17);
 	$newsOld = htmlentities($_POST['meldung2']);
+	$currentProf = htmlentities($_POST['prof']);
+	
+	// wählt die Datei abhängig vom ausgewählten Professor aus
+	switch($currentProf) {
+		case 'korte':
+			// $fileName = 'Korte_status.csv';			// Speicherort im gleichen Ort
+			$fileName = '../Korte_status.csv';		// Speicherort einen Ordner darüber
+			break;
+		case 'hausdoerfer':
+			// $fileName = 'Hausdoerfer_status.csv';		// Speicherort im gleichen Ort
+			$fileName = '../Hausdoerfer_status.csv';		// Speicherort einen Ordner darüber
+			break;
+		default:
+			// $fileName = 'status.csv';		// Speicherort im gleichen Ort
+			$fileName = '../status.csv';		// Speicherort einen Ordner darüber
+			break;
+	}	
+	
+	echo "Übertragene Daten werden gespeichert...\n";	
 	
 	// Wenn der Zustand nicht übertragen wurde, hat die gesamte Übertragung nicht geklappt
 	if(!empty($state)) {
 		// Wenn eine leere neue Meldung ODER die Standard-Meldung übertragen wurde, sollen die Meldungen nicht geändert werden
 		if(empty($newsWithoutDate) || (strcmp($newsWithoutDate, $defaultText) === 0)) {
-			$dataToWrite = "\"".$state."\",\"".$newsNew."\",\"".$newsOld."\"";
+			$dataToWrite = $state.", ".$newsNew.", ".$newsOld;
 			echo "Keine neue Meldung eingetragen\n";
 		//	Wenn die neue Meldung gleich der ersten alten Meldung ist, dann sollen diese ebenfalls nicht geändert werden
 		} else if(strcmp($newsWithoutDate, $newsNewWithoutDate) === 0) {
-			$dataToWrite = "\"".$state."\",\"".$newsNew."\",\"".$newsOld."\"";
+			$dataToWrite = $state.", ".$newsNew.", ".$newsOld;
 			echo "Die gleiche Meldung wie beim letzten Mal\n";
 		// Die neue Meldung ist ungleich der alten Meldung, also wird die neue Meldung hinzugefügt
 		} else if(strcmp($newsWithoutDate, $newsNewWithoutDate) !== 0){
-			$dataToWrite = "\"".$state."\",\"".$newsToWrite."\",\"".$newsNew."\"";
+			$dataToWrite = $state.", ".$newsToWrite.", ".$newsNew;
 			echo "Neue Meldung!\n";
 		}
 		
